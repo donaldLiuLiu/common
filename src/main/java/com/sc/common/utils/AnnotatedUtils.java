0006bb19bdbcb2fefa64202f976850e9a304e876
fresh <1326191992@qq.com>
2020-11-16 18:13:43 +0800

    tt

---
+++ src/main/java/com/sc/common/utils/AnnotatedUtils.java
@@ -0,0 +1,14 @@
+package com.sc.common.utils;
+
+public abstract class AnnotatedUtils {
+
+
+    //Class.getAnnotatedSuperclass,Class.getAnnotatedInterfaces
+    //Constructor.getAnnotatedReturnType(),Constructor.getAnnotatedReceiverType()
+    //Method.getAnnotatedReturnType
+    //Field.getAnnotatedType
+    //AnnotatedElement
+    //AnnotationAttribute
+    //core.annotation
+
+}
