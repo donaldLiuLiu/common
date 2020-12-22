effe098838e75226622985c50847882b2321a4de
fresh <1326191992@qq.com>
2020-12-22 23:31:45 +0800

    注释

--- src/main/java/com/sc/common/entity/BaseEntity.java
+++ src/main/java/com/sc/common/entity/BaseEntity.java
@@ -9,10 +9,10 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.Getter;
 import lombok.Setter;
-
+import org.springframework.format.annotation.DateTimeFormat;
 import java.io.Serializable;
 import java.time.LocalDateTime;
-import java.time.ZoneOffset;
+import java.util.Date;
 
 @Getter
 @Setter
@@ -24,21 +24,16 @@ public abstract class BaseEntity<ID> implements Serializable {
 	@JsonSerialize(using = ToStringSerializer.class)
 	@TableId
 	private ID id;
-	
-	//@JsonFormat(pattern= "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
-    //@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-	//private Date createTime = new Date();
 
-	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss", timezone="GMT+8")
-	//private LocalDateTime createTime = LocalDateTime.now(ZoneOffset.of("+8"));
+	/** 日期类型保存到数据库和下面的两个注解没有关系，mysql-connector驱动包中默认使用SimpleDateFormat[yyyy-MM-dd][yyyy-MM-dd HH:mm:ss]将日期转化成String，数据库中存的日期字符串格式即此 */
+	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss", timezone="GMT+8")  //web中application/json 序列化与反序列化
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")  //web中form表单 序列化与反序列化
+	//private Date createTime;
 	private LocalDateTime createTime;
 
-	//@JsonFormat(pattern= "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
-    //@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-	//private Date modifyTime = new Date();
-
-	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss", timezone="GMT+8")
-	//private LocalDateTime modifyTime = LocalDateTime.now(ZoneOffset.of("+8"));
+	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss", timezone="GMT+8")  //web中application/json 序列化与反序列化
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")  //web中form表单 序列化与反序列化
+	//private Date modifyTime;
 	private LocalDateTime modifyTime;
 
 }
