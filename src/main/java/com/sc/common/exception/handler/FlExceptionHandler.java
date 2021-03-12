ef550eec1961adc30f08faa4a7dbb7b9898c9a29
fresh <1326191992@qq.com>
2021-03-12 18:23:52 +0800

    1

--- src/main/java/com/sc/common/exception/handler/FlExceptionHandler.java
+++ src/main/java/com/sc/common/exception/handler/FlExceptionHandler.java
@@ -7,15 +7,22 @@ import com.sc.common.exception.BizException;
 import com.sc.common.vo.JsonResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 
 @Slf4j
 @ControllerAdvice
 public class FlExceptionHandler {
 
+    /**
+     * Biz异常
+     * @param ex
+     * @return
+     */
     @ExceptionHandler(BizException.class)
     @ResponseBody
     public JsonResult handlerBizException(BizException ex) {
@@ -23,11 +30,29 @@ public class FlExceptionHandler {
     }
 
 
+    /**
+     * 参数校验异常
+     * @param ex
+     * @return
+     */
     @ExceptionHandler(BindException.class)
     @ResponseBody
     public JsonResult handlerBindException(BindException ex) {
         return JsonResult.buildFailedResult(JsonResultEnum.FAIL.getCode(),
                                             ex.getBindingResult().getFieldError().getDefaultMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    public JsonResult handlerValidateException(MethodArgumentNotValidException  ex) {
+        return JsonResult.buildFailedResult(JsonResultEnum.FAIL.getCode(),
+                                            ex.getBindingResult().getFieldError().getDefaultMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseBody
+    public JsonResult handlerArgTypeException(MethodArgumentTypeMismatchException e) {
+        return JsonResult.buildFailedResult(JsonResultEnum.FAIL.getCode(), CommonConstants.ARG_TYPE_ERROR);
     }
 
 
